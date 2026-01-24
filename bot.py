4c563cc96cbe9b1103c864f1e974dfc761726824
davidbernath8-dotcom <davidbernath8@gmail.com>
2026-01-24 20:20:22 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,21 +4,45 @@ from discord import app_commands
 import os
 
 intents = discord.Intents.default()
+intents.guilds = True
+intents.members = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-GUILD_ID = 123456789012345678  # A SZERVER ID
+GUILD_ID = 123456789012345678  # szerver ID-d
+STAFF_ROLE_NAME = "<@&1463254825256091761> , <@&1463254505700462614>  "      # staff role neve
+
+ticket_count = 0  # egyszerű számláló (újraindításkor nullázódik)
 
 @bot.event
 async def on_ready():
     guild = discord.Object(id=GUILD_ID)
     bot.tree.copy_global_to(guild=guild)
     await bot.tree.sync(guild=guild)
-    print("Bot elindult")
+    print("Bot ONLINE")
 
-@bot.tree.command(name="ticket", description="Ticket nyitása")
+@bot.tree.command(name="ticket", description="Nyiss egy ticketet")
 async def ticket(interaction: discord.Interaction):
-    await interaction.response.send_message(
-        "🎟️ Ticket parancs működik!", ephemeral=True
+    global ticket_count
+    ticket_count += 1
+    guild = interaction.guild
+
+    # Jogosultságok
+    overwrites = {
+        guild.default_role: discord.PermissionOverwrite(read_messages=False),
+        interaction.user: discord.PermissionOverwrite(read_messages=True, send_messages=True),
+    }
+
+    staff_role = discord.utils.get(guild.roles, name=<@&1463254825256091761> , <@&1463254505700462614> )
+    if staff_role:
+        overwrites[<@&1463254825256091761>] = discord.PermissionOverwrite(read_messages=True, send_messages=True)
+
+    # Csatorna létrehozás
+    channel_name = f"ticket-{ticket_count}"
+    ticket_channel = await guild.create_text_channel(
+        name=channel_name,
+        overwrites=overwrites
     )
+
+    await interaction.response.send_message(f"🎟️ Ticket létrehozva: {ticket_channel.mention}", ephemeral=True)
 
 bot.run(os.getenv("TOKEN"))
